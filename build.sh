9108db0ed7f43d8a1bbc37a8a14cdd0be3ac6289
Mox Soini <mox@iki.fi>
2017-10-15 14:51:44 +0300

    Build dependency framework

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 TRG=11.0
 PKG=ProtoKit
+THEN=Then
 COMMONARGS="-configuration Release -project $PKG.xcodeproj -target $PKG IPHONEOS_DEPLOYMENT_TARGET=$TRG"
 rm -rf ./.build
 
@@ -21,12 +22,20 @@ xcodebuild -arch $ARCH -sdk $SDK $COMMONARGS TARGET_BUILD_DIR=$BUILDDIR FRAMEWOR
 
 # copy x86_64 module maps
 cp ./.build/build-x86/$PKG.framework/Modules/$PKG.swiftmodule/* ./.build/$PKG.framework/Modules/$PKG.swiftmodule/
+cp ./.build/build-x86/$THEN.framework/Modules/$THEN.swiftmodule/* ./.build/$THEN.framework/Modules/$THEN.swiftmodule/
 
-# lipo archs together
+# lipo PKG archs together
 cd ./.build/$PKG.framework/
 mv $PKG ${PKG}-arm
 lipo -create -output $PKG ${PKG}-arm ../build-x86/$PKG.framework/$PKG
 rm ${PKG}-arm
+cd ../..
+
+# lipo THEN archs together
+cd ./.build/$THEN.framework/
+mv $THEN ${THEN}-arm
+lipo -create -output $THEN ${THEN}-arm ../build-x86/$THEN.framework/$THEN
+rm ${THEN}-arm
 cd ../..
 
 # Clean up intermediary build files
