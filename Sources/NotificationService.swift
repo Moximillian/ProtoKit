d5d70520fafefca9daf51ed01e398b288fc6faaf
Mox Soini <mox@iki.fi>
2019-04-28 19:03:21 +0300

    Rename Observer => NotificationService

--- Sources/NotificationService.swift
+++ Sources/NotificationService.swift
@@ -17,7 +17,7 @@
 import Foundation
 
 /// Register Notification Observers using closure, comes with automatic observer removal
-public final class Observer {
+public final class NotificationService {
   private var observers: [NSObjectProtocol] = []
   private let queue: OperationQueue
 
@@ -29,7 +29,7 @@ public final class Observer {
     observers.forEach { NotificationCenter.default.removeObserver($0) }
   }
 
-  public func add(_ name: Notification.Name, using block: @escaping (Notification) -> Void) {
+  private func listen(to name: Notification.Name, using block: @escaping (Notification) -> Void) {
     let observer = NotificationCenter.default.addObserver(forName: name, object: nil, queue: queue, using: block)
     observers.append(observer)
   }
@@ -37,18 +37,17 @@ public final class Observer {
 
 /// generic struct that holds notifications containing a value of specific type
 public struct TypedNotification<ObjectType> {
-  private var title: NSString
+  public private(set) var name: Notification.Name
 
-  public init(title: NSString) {
-    self.title = title
+  public init(title: String) {
+    name = NSString(string: title) as Notification.Name
   }
-  public var name: Notification.Name { return title as Notification.Name }
 }
 
-extension Observer {
+extension NotificationService {
   /// add listening notification that acts on predefined (typed) value
-  public func add<T>(_ notification: TypedNotification<T>, using block: @escaping (T) -> Void) {
-    add(notification.name) { notification in
+  public func listen<T>(to notification: TypedNotification<T>, using block: @escaping (T) -> Void) {
+    listen(to: notification.name) { notification in
       guard let value = notification.userInfo?[notification.name] as? T else {
         fatalError("Observer: invalid TypedNotification value type")
       }
@@ -74,23 +73,23 @@ extension Observer {
    static var note2: TypedNotification<String> { return .init(title: "myNotification2") }
  }
 
-// 2a.For listening to notifications instantiate observer inside your viewcontroller
-// 2b. ...and add the notification
+// 2a.For listening to notifications instantiate NotificationService inside your viewcontroller
+// 2b. ...and listen to the notification
 
   class myVC: UIViewController {
-    let observer = Observer()
+    let notificationService = NotificationService()
 
     override func viewDidLoad() {
       super.viewDidLoad()
 
-      observer.add(MyNotifications.note1) { [weak self] value in
+      notificationService.listen(to: MyNotifications.note1) { [weak self] value in
         // do stuff with value
       }
     }
   }
 
-// 3. For posting notifications, just use Observer.post()
+// 3. For posting notifications, just use NotificationService.post()
 
-  Observer.post(MyNotifications.note1, value: 2)
+  NotificationService.post(MyNotifications.note1, value: 2)
 
 */
